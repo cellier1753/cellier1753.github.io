9b7840ba540289e00c29859eab6204b8203c5583
cellier1753 <cellier@cellierdeMacBook-Pro.local>
2017-04-09 01:10:28 +0800

    shengbixing.com

---
+++ js/shengbixing.js
@@ -0,0 +1,13 @@
+
+
+//
+// $(document).ready(function(){
+//     $('body').delay(500); // 1000等于1秒
+// });
+
+
+$(document).ready(function(){
+        $(".sider").delay("200").fadeIn(2000);
+        $(".main").delay("2000").fadeIn(6000);
+});
+
